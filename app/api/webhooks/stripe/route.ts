8d94ddee4b165e0e627aa6beaab858354b639a26
Martin Rothe <martin.r.rothe@gmail.com>
2024-09-08 15:56:29 +0200

    stripe signature

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,8 +2,17 @@ import stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order.actions'
 
+const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY!)
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+  }
+
 export async function POST(request: Request) {
-  const body = await request.text()
+    const body = await request.clone().arrayBuffer()
+    const rawBody = Buffer.from(body)
 
   const sig = request.headers.get('stripe-signature') as string
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
@@ -11,7 +20,7 @@ export async function POST(request: Request) {
   let event
 
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+    event = stripeInstance.webhooks.constructEvent(rawBody, sig, endpointSecret)
   } catch (err) {
     return NextResponse.json({ message: 'Webhook error', error: err })
   }
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -11,8 +11,8 @@ import {ObjectId} from 'mongodb';
 import User from '../database/models/user.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
-  
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
   const price = order.isFree ? 0 : Number(order.price) * 100;
 
   try {
